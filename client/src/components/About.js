c0fb20072e91b16d10bba00d46c6978a79b57486
codeviith <codeviith@gmail.com>
2023-12-28 22:20:46 -0500

    updated about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,23 +5,15 @@ import DIY_collage from './images/p5 project collage.jpg';
 function About() {
     return (
       <div className="about">
-        <h5>Welcome to DIYesk8Guide: Your One-Stop Site for DIY Builds!</h5>
         <span style={{ whiteSpace: 'pre-wrap' }}>
-          Are you ready to elevate your skateboarding experience to a whole new level?{'\n'}
-        </span>
-        <span id="special-font1" style={{ whiteSpace: 'pre-wrap' }}>
-          Well, look no further!{'\n'}
+          Welcome to DIYesk8Guide, the hub for aspiring DIY electric skateboard builders. Created by skate enthusiasts for beginners, our aim is to equip you with essential knowledge and skills to build your electric skateboard from the ground up.{'\n'}
         </span>
         <span style={{ whiteSpace: 'pre-wrap' }}>
-          Our DIY Electric Skateboard Guide is your gateway to the exciting world 
-          of crafting your own electric-powered ride. Whether you're a seasoned skateboarder seeking an 
-          electrifying upgrade or a DIY enthusiast eager to embark on a thrilling project, this is the place for you.{'\n'}
+          Our journey is rooted in a deep love for skateboarding and the desire to empower others to experience the thrill of crafting their own electric ride. Here at DIYesk8Guide, we offer detailed guides, step-by-step instructions, and a supportive community where passion and skate culture thrive.{'\n'}
         </span>
-        <span id="special-font2" style={{ whiteSpace: 'pre-wrap' }}>
-          So unleash your potential and build like a PRO!</span>
-          <div className="about_collage">
-            <img src={DIY_collage} alt="DIY_collage" className="collage" />
-          </div>
+        <span style={{ whiteSpace: 'pre-wrap' }}>
+        Embark on your building adventure with us, and transform your skateboarding experience. DIYesk8Guide isn't just a guide; it's your gateway to a community united by the shared joy of DIY creation.{'\n'}
+        </span>
       </div>
     );
 }
