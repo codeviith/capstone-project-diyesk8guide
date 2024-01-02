a11726d1ef084d75ac59c994f6cf61f1de176c4c
codeviith <codeviith@gmail.com>
2024-01-02 02:01:09 -0500

    updated gallery css

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 function Gallery() {
     const [image, setImage] = useState(null);
-    const [preview, setPreview] = useState('');
     const [dropdowns, setDropdowns] = useState({ menu1: '', menu2: '', menu3: '', menu4: '', menu5: '' });
     const [galleryItems, setGalleryItems] = useState([]);
+    const [fileName, setFileName] = useState('');
+
 
     useEffect(() => {
         const fetchGalleryItems = async () => {
@@ -24,12 +25,15 @@ function Gallery() {
         const file = e.target.files[0];
         if (file && file.type.startsWith('image/')) {
             setImage(file);
-            setPreview(URL.createObjectURL(file));
+            // setPreview(URL.createObjectURL(file));
+            setFileName(file.name); // Set the file name
         } else {
             setImage(null);
-            setPreview('');
+            // setPreview('');
+            setFileName(''); // Clear the file name
         }
     };
+
 
     const handleDropdownChange = (e) => {
         setDropdowns({ ...dropdowns, [e.target.name]: e.target.value });
@@ -53,7 +57,7 @@ function Gallery() {
 
     return (
         <div className="gallery-container">
-            {/* Gallery items section */}
+            {/* Gallery items */}
             <div className="gallery-items">
                 {galleryItems.map((item, index) => (
                     <div key={index} className="gallery-item">
@@ -69,10 +73,14 @@ function Gallery() {
                 ))}
             </div>
 
-            {/* Floating form section */}
+            {/* Floating form */}
             <div className="floating-form">
                 <form onSubmit={handleSubmit} encType="multipart/form-data">
-                    <input type="file" accept="image/*" onChange={handleImageChange} />
+                    {fileName && <div className="file-name-display">{fileName}</div>}
+                    <div className="file-input-wrapper">
+                        <input type="file" accept="image/*" onChange={handleImageChange} id="file-input" style={{ display: 'none' }} />
+                        <label htmlFor="file-input" className="file-input-button">Select Image</label>
+                    </div>
                     <br />
                     {[1, 2, 3, 4, 5].map(num => (
                         <select key={num} name={`menu${num}`} value={dropdowns[`menu${num}`]} onChange={handleDropdownChange}>
@@ -91,139 +99,6 @@ function Gallery() {
     );
 }
 
-
-
 export default Gallery;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-
-// function Gallery() {
-//     const [image, setImage] = useState(null);
-//     const [dropdowns, setDropdowns] = useState({ menu1: '', menu2: '', menu3: '', menu4: '', menu5: '' });
-//     const [galleryItems, setGalleryItems] = useState([]); // State to store uploaded pictures
-
-
-//     const handleImageChange = (e) => {
-//         const file = e.target.files[0];
-//         if (file && file.type.startsWith('image/')) {
-//             setImage(file);
-//             setPreview(URL.createObjectURL(file));
-//         } else {
-//             setImage(null);
-//             setPreview('');
-//         }
-//     };
-
-//     const handleDropdownChange = (e) => {
-//         setDropdowns({ ...dropdowns, [e.target.name]: e.target.value });
-//     };
-
-//     const handleSubmit = async (e) => {
-//         e.preventDefault();
-//         const formData = new FormData();
-//         formData.append('image', image);
-//         Object.keys(dropdowns).forEach(key => formData.append(key, dropdowns[key]));
-
-//         // Send data to Flask backend
-//         const response = await fetch('/gallery', {
-//             method: 'POST',
-//             body: formData,
-//         });
-//         const responseData = await response.json();
-//         // console.log(responseData);
-//     };
-
-//     return (
-//         <div className="gallery">
-//             <form onSubmit={handleSubmit} encType="multipart/form-data">
-//                 <input type="file" accept="image/*" onChange={handleImageChange} />
-//                 <br />
-
-//                 {[1, 2, 3, 4, 5].map(num => (
-//                     <select key={num} name={`menu${num}`} value={dropdowns[`menu${num}`]} onChange={handleDropdownChange}>
-//                         <option value="">Select Option</option>
-//                         <option value="Option1">Option 1</option>
-//                         <option value="Option2">Option 2</option>
-//                         <option value="Option3">Option 3</option>
-//                         <option value="Option4">Option 4</option>
-//                     </select>
-//                 ))}
-
-//                 <br />
-//                 <button type="submit">Submit</button>
-//             </form>
-
-//             {/* Display gallery items */}
-//             <div className="gallery-items">
-//                 {galleryItems.map((item, index) => (
-//                     <div key={index} className="gallery-item">
-//                         <img src={item.image} alt="Gallery item" />
-//                         <div>
-//                             {Object.entries(item.dropdowns).map(([key, value]) => (
-//                                 <p key={key}>{`${key}: ${value}`}</p>
-//                             ))}
-//                         </div>
-//                     </div>
-//                 ))}
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Gallery;
 
