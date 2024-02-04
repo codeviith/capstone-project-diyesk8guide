ec4b389bb5c34ac17f5071d3c3f2e0f8467d64ea
codeviith <codeviith@gmail.com>
2024-02-03 23:29:01 -0500

    updated Donations title

--- client/src/components/Donations.js
+++ client/src/components/Donations.js
@@ -6,7 +6,7 @@ function Donations() {
     return (
         <div className="donation">
             <h1 className="donation-title" style={{ whiteSpace: 'pre-wrap' }}>
-                Become a Donor: Help contribute to the cause {'\n'}
+                Support the Future of E-Board Building: Contribute Today {'\n'}
             </h1>
             <span style={{ whiteSpace: 'pre-wrap' }}>
                 Join us today in shaping the future of the DIY electric skateboard community! Your support is crucial in nurturing a sustainable and vibrant community where creativity soars and knowledge flows freely. Every contribution you make fuels our mission, directly impacting our ability to educate, inspire, and empower. Your donations drive the enhancement and longevity of our platform, facilitating future upgrades of sophisticated AI-powered tools, improved dynamic builders' gallery, and more advanced build generator. These improvements are designed to enrich the user experience and expand access to valuable knowledge.{'\n'}{'\n'}
@@ -15,7 +15,7 @@ function Donations() {
                 By supporting our cause, you do more than contribute to a website; you play a pivotal role in fostering a culture of innovation and mutual growth within the DIY e-boarding world. Together, we are broadening the horizons for electric skateboard building, making it more accessible and enjoyable for everyone. Your support ensures that the spirit of DIY and creativity not only survives but thrives.{'\n'}{'\n'}
             </span>
             <div className="donation-button-container">
-                <h2>Ready to Donate?</h2>
+                <h2>Thank You for Your Support</h2>
                 <p>Please click the button below. You will be redirected to a secured external site.</p>
                 <a href="https://pay.diyesk8guide.com/" className="donation-button" target="_blank" rel="noopener noreferrer">Make a Donation</a>
             </div>
