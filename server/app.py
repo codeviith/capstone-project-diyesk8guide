25942e7851fc99b627f82585d9f7aed9a15dd817
codeviith <codeviith@gmail.com>
2024-03-08 00:18:12 -0500

    updated wildcard with frontend url, added 'PATCH' method

--- server/app.py
+++ server/app.py
@@ -62,10 +62,10 @@ bcrypt.init_app(app)
 
 @app.after_request
 def after_request_func(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')  # or a specific domain instead of '*'
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    response.headers.add('Access-Control-Allow-Credentials', 'true')  # If you need to support cookies across domains
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
     return response
 
 
