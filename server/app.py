5ffbce54f84e4d7561c526f4222c9fa98839801d
codeviith <codeviith@gmail.com>
2024-03-11 14:37:35 -0400

    added logging code for debugging

--- server/app.py
+++ server/app.py
@@ -185,12 +185,12 @@ def login():
         user = User.query.filter_by(email=data['email']).first()
         if user and bcrypt.check_password_hash(user.password_hash, data['password']):
             session['user_id'] = user.id
-            app.logger.info('User logged in: %s', user.id)  ### code to log successul login
+            print('User logged in: %s', user.id)  ### code to log successul login
             return jsonify({'success': True, 'message': 'Logged in successfully'}), 200
         else:
             return jsonify({'success': False, 'message': 'Invalid email or password'}), 401
     except Exception as e:
-        app.logger.error(f"Login error: {e}")  ## code to log unsuccessful login
+        print(f"Login error: {e}")  ## code to log unsuccessful login
         return jsonify({'error': 'Internal Server Error', 'message': str(e)}), 500
 
 
