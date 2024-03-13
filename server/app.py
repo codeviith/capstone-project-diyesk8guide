31c49ee66f8e730ac7d7a3f82ec0a46cc52e55af
codeviith <codeviith@gmail.com>
2024-03-13 01:18:39 -0400

    errors, broken images, reverting back

--- server/app.py
+++ server/app.py
@@ -55,17 +55,17 @@ app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
 client = OpenAI(api_key=openai_api_key)
 
 # Instantiate CORS
-# CORS(app, supports_credentials=True, resources={r"/*": {"origins": "https://diyesk8guide-frontend.onrender.com"}})
-CORS(app, resources={
-    r"/*": {
-        "origins": ["https://diyesk8guide-frontend.onrender.com"],
-        "methods": ["GET"],  ### code for allowed methods
-        "allow_headers": ["*"],  ### code for allowed headers
-        "expose_headers": [],  ### code for headers that browsers are allowed access to
-        "supports_credentials": True,
-        "max_age": 3000,  ### code for maximum cache age for preflight requests
-    }
-})
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "https://diyesk8guide-frontend.onrender.com"}})
+# CORS(app, resources={
+#     r"/*": {
+#         "origins": ["https://diyesk8guide-frontend.onrender.com"],
+#         "methods": ["GET"],  ### code for allowed methods
+#         "allow_headers": ["*"],  ### code for allowed headers
+#         "expose_headers": [],  ### code for headers that browsers are allowed access to
+#         "supports_credentials": True,
+#         "max_age": 3000,  ### code for maximum cache age for preflight requests
+#     }
+# })
 
 # configure session
 # app.config['SESSION_TYPE'] = 'sqlalchemy'
