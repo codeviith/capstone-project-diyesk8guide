bde541fa2fc1ddcef0dc7b3f664d928e9cabc081
KLin <codeviith@gmail.com>
2024-03-19 19:36:31 -0400

    index.html, navBar, index.css

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -38,13 +38,14 @@ function NavBar() {
                 <NavLink to="/guru">Guru</NavLink>
                 {/* <NavLink to="/qna">Q&A</NavLink> */}
                 <NavLink to="/gallery">Gallery</NavLink>
-
             </div>
+
+            <button class="hamburger" aria-label="Toggle menu">☰</button>
+
             <div className="nav-links-right"> {/* Container for right-hand side links */}
                 {/* Menu Item to toggle dropdown */}
                 <div className="menu-item" onClick={toggleMenu}>
                     <MenuIcon />
-
                     {/* Dropdown Menu */}
                     {showMenu && (
                         <div className={`dropdown-menu ${showMenu ? 'show-dropdown' : ''}`}>
