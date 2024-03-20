e1e638ef5f558bbb563fdc536149dd2328d07f55
KLin <codeviith@gmail.com>
2024-03-20 01:24:02 -0400

    more updates on guide

--- client/src/components/Guide.js
+++ client/src/components/Guide.js
@@ -17,23 +17,35 @@ const Guide = () => {
 
     // Step descriptions
     const stepDescriptions = [
-        "So you've been wanting to make an electric skateboard for quite some time now but " +
-        "you don't know where to start. Well, the first thing to do is to make a game plan " +
-        "that documents the expected process of your build." +
+        //Step 1
+        "So you've been wanting to make an electric skateboard for quite some time now but you don't know where to start. " +
+        "Well, the first thing to do is to make a game plan that documents the expected process of your build. " +
         "This means designing and drawing out sketches, jotting down any ideas that pop into mind, and doing research " +
-        "on electric skateboards. This is a must in your e-board building journey and we cannot stress" +
-        "how important it is. Once you have a game plan and a design in mind, you can then move on to researching for parts.", //Step 1
-        "This is one of the most crucial steps in your eboard building process: selecting for parts. A top performing" +
-        "and reliable eboard depends on the material that it is built from. This is the step that requires the most time" +
-        "and researh on. Higher quality parts means less time spent on fixing breakdowns and more time riding." +
-        "We always advicenew builders to take this step seriously and select reputable parts.", //Step 2
-        "Description for Step 3: Further details...", //Step 3
-        "Description for Step 4...", //Step 4
-        "Description for Step 5...", //Step 5
-        "Description for Step 6...", //Step 6
-        "Description for Step 7...", //Step 7
-        "Description for Step 8...", //Step 8
-        "Description for Step 9..." //Step 9
+        "on electric skateboards. This is a must in your e-board building journey and we cannot stress " +
+        "how important it is. Once you have a game plan and a design in mind, you can then move on to researching for parts. ",
+        //Step 2
+        "This is one of the most crucial steps in your eboard building process. A top performing " +
+        "and reliable eboard depends on the material that it is built from. This is the step that requires the most time " +
+        "and researh on. Higher quality parts equates to less time spent on fixing breakdowns and more time riding. " +
+        "We always advice new builders to take this step seriously and purchase the parts from merchants you can trust.",
+        //Step 3
+        "Once you have decided on the design and purchased the parts for your build, the next step is to establish a distraction-" +
+        "free, if possible, workshop where you will spend the next few weeks, or even months, planning, building, and testing your " +
+        "board. Generally, a large tabletop complemented with a rolling stool and quick access to tools and/or machineries would " +
+        "be sufficient. As a word of caution, please make sure the workshop is well ventilated as you will be working with wood/metal " +
+        "dust and solder fumes throughout the build. Please remember that safety should always be your number one priority."
+        //Step 4
+        "Description for Step 4...",
+        //Step 5
+        "Description for Step 5...",
+        //Step 6
+        "Description for Step 6...",
+        //Step 7
+        "Description for Step 7...",
+        //Step 8
+        "Description for Step 8...",
+        //Step 9
+        "Description for Step 9..."
     ];
 
     // Step href links
