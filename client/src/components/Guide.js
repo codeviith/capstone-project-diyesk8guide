68d5b5755f288d01e212ad4b1f4f0a3a1b996d1e
KLin <codeviith@gmail.com>
2024-03-21 00:41:49 -0400

    removed duplicate code for step 4

--- client/src/components/Guide.js
+++ client/src/components/Guide.js
@@ -35,9 +35,7 @@ const Guide = () => {
         "be sufficient. As a word of caution, please make sure the workshop is well ventilated as you will be working with wood/metal " +
         "dust and solder fumes throughout the build. Please remember that safety should always be your number one priority.",
         //Step 4
-        """'Man is a tool-using animal. Without tools he is nothing, with tools he is all.' ~Thomas Carlyle.<br />" + //want a new line here
-        "This is yet another important step in the eboard journey. Having the right tools and materials is what sets apart a successful " +
-        "project versus an unsuccessful one. Here are a few of the recommended tools for starters: """,
+        "",
         //"list of tools here...and maybe with pictures??? not sure yet",
         //Step 5
         "Description for Step 5...",
