cc4b1a2aafa4bc0b0aab2c9f14327f2b66861fac
KLin <codeviith@gmail.com>
2024-03-23 04:19:39 -0400

    adding session check, reverting

--- client/src/components/AuthContext.js
+++ client/src/components/AuthContext.js
@@ -76,7 +76,7 @@ export const AuthProvider = ({ children }) => {
         };
 
         checkLoginStatus();
-    }, []);
+    }, [backendUrl]);
 
     useEffect(() => {
         const handleActivity = () => {
