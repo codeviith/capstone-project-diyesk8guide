cbd4bd801f4788c8270675db085d4463225447ab
KLin <codeviith@gmail.com>
2024-03-28 02:39:32 -0400

    css disclaimer, rules policies take 4

--- client/src/components/RulesAndPolicies.js
+++ client/src/components/RulesAndPolicies.js
@@ -35,45 +35,63 @@ function RulesAndPolicies() {
                 you follow these content guidelines:{'\n'}{'\n'}
             </span>
             <span style={{ whiteSpace: 'pre-wrap' }}>
-                Relevance: Only post content that is directly related to electric skateboard building. The Gallery is not a space for unrelated
-                images or discussions.{'\n'}{'\n'}
-                Prohibited Content: The following types of content are strictly prohibited and will be removed:{'\n'}{'\n'}
+                <ul>
+                    <li>
+                        Relevance: Only post content that is directly related to electric skateboard building. The Gallery is not a space for unrelated
+                        images or discussions.
+                    </li>
+                    <li>
+                        Prohibited Content: The following types of content are strictly prohibited and will be removed:
+                        <ul>
+                            <li>
+                                Unlawful Content: Any content that promotes illegal activities or violates laws.
+                            </li>
+                            <li>
+                                Explicit Content: Pornography, graphic violence, or any other adult content.
+                            </li>
+                            <li>
+                                Hateful Content: Anything promoting hate speech, discrimination, or violence against individuals or groups based on
+                                race, ethnicity, religion, gender, sexual orientation, disability, or any other characteristics.
+                            </li>
+                            <li>
+                                Personal Information: Do not share personal information, either yours or someone else’s. This includes, but is not
+                                limited to, addresses, phone numbers, and email addresses.
+                            </li>
+                            <li>
+                                Copyrighted Material: Only upload images that you own or have the right to use. By submitting content, you are
+                                responsible for ensuring that it does not infringe on the copyrights or rights of others.
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        Reporting Violations: If you encounter any content that violates these guidelines, please use the report function to alert
+                        our moderators. We are committed to taking appropriate actions, which may include removing content or suspending user accounts.
+                    </li>
+                </ul>
             </span>
             <strong>
-                General Disclaimer
+                Privacy Guidelines
             </strong>
             <span style={{ whiteSpace: 'pre-wrap' }}>
-                Rules and Policies text here{'\n'}{'\n'}
+                Protecting your privacy and the privacy of others is essential. Please refrain from posting private information in any public areas
+                of the site, including the Gallery. Be mindful of the information you share and consider the potential risks involved in disclosing
+                personal details.{'\n'}{'\n'}
             </span>
             <strong>
-                General Disclaimer
+                Policy Enforcement
             </strong>
             <span style={{ whiteSpace: 'pre-wrap' }}>
-                Rules and Policies text here{'\n'}{'\n'}
+                Violation of these rules and policies may result in content removal, account suspension, or other actions deemed necessary to ensure
+                the safety and integrity of our community. We reserve the right to enforce these policies at our discretion and to modify them at
+                any time.{'\n'}{'\n'}
             </span>
             <strong>
-                General Disclaimer
+                Contact Us
             </strong>
             <span style={{ whiteSpace: 'pre-wrap' }}>
-                Rules and Policies text here{'\n'}{'\n'}
-            </span>
-            <strong>
-                General Disclaimer
-            </strong>
-            <span style={{ whiteSpace: 'pre-wrap' }}>
-                Rules and Policies text here{'\n'}{'\n'}
-            </span>
-            <strong>
-                General Disclaimer
-            </strong>
-            <span style={{ whiteSpace: 'pre-wrap' }}>
-                Rules and Policies text here{'\n'}{'\n'}
-            </span>
-            <strong>
-                General Disclaimer
-            </strong>
-            <span style={{ whiteSpace: 'pre-wrap' }}>
-                Rules and Policies text here{'\n'}{'\n'}
+                If you have questions about these rules and policies or need to report a violation, please contact
+                us <NavLink className="contactus-href" to="/contact-us">here</NavLink>. Your feedback is important to us as we
+                strive to create a welcoming and safe environment for all users.{'\n'}{'\n'}
             </span>
             <div className="footer-bottom">
                 <NavLink className="footer-bottom-link" to="/about">About</NavLink>
