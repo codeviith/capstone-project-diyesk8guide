e00d6e15eb2a60d0e0a78e0dc3079b8fa16a0a71
KLin <codeviith@gmail.com>
2024-04-04 23:49:17 -0400

    added backdrop overlay to disable contents for modal take 2 for gallery

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const Modal = ({ isOpen, onClose, onConfirm, message1, message2, message3 }) => {
+    const firstActionButtonRef = useRef(null);
+
+    useEffect(() => {
+        if (isOpen) {  // code to focus on the first actionable button when the modal is active
+            firstActionButtonRef.current.focus();
+        }
+    }, [isOpen]); // here, the dependency array includes isOpen so as to trigger the effect on isOpen change
+
     if (!isOpen) return null;
+
 
     return (
         <div className="modal-overlay">
@@ -10,7 +19,9 @@ const Modal = ({ isOpen, onClose, onConfirm, message1, message2, message3 }) => 
                 <p className='modal-text'>{message2}</p>
                 <p className='modal-text'>{message3}</p>
                 <div className="modal-actions">
+                    {/* This is where useRef is applied to the "Agree & Proceed" button */}
                     <button className='modal-button-agree'
+                        ref={firstActionButtonRef}
                         onClick={onConfirm}
                     >Agree & Proceed
                     </button>
