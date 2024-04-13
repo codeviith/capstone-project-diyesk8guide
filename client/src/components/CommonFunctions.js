11c4e83c070b91f1ea833fca3ba7600a94ba94d3
KLin <codeviith@gmail.com>
2024-04-12 23:26:27 -0400

    adj guru response style take 1

--- client/src/components/CommonFunctions.js
+++ client/src/components/CommonFunctions.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
-export const formatResponse = (response) => {
-    const lines = response.split('\n');
+// export const formatResponse = (response) => {
+//     const lines = response.split('\n');
 
-    return lines.map((line, index) => (
-        <React.Fragment key={index}>
-            {line}
-            <br />
-        </React.Fragment>
+//     return lines.map((line, index) => (
+//         <React.Fragment key={index}>
+//             {line}
+//             <br />
+//         </React.Fragment>
+//     ));
+// }
+
+/// trying a new logic
+export const formatResponse = (response) => {
+    const paragraphs = response.split('\n').filter(paragraph => paragraph.trim() !== '');
+
+    return paragraphs.map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
     ));
 }
-
-
