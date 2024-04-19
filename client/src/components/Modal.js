435781806795a8a35c93541f6b034ac5751bbbde
KLin <codeviith@gmail.com>
2024-04-19 03:38:31 -0400

    adjusting gallery modal to disable touch scrolling take 1

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -4,15 +4,24 @@ const Modal = ({ isOpen, onClose, onConfirm, message1, message2, message3 }) => 
     const onlyActionRefButton = useRef(null);
 
     useEffect(() => {
+        function handleTouchMove(e) {
+            if (isOpen) {
+                e.preventDefault();
+            }
+        }
+
         if (isOpen) {  // code to focus on the first actionable button when the modal is active
             onlyActionRefButton.current.focus();
             document.body.style.overflow = 'hidden';  // code to disable scrolling when modal is active
+            document.addEventListener('touchmove', handleTouchMove, { passive: false });  // code to disable scrolling when modal is active for mobile devices
         } else {
             document.body.style.overflow = '';  // code to re-enable scrolling when modal is inactive
+            document.removeEventListener('touchmove', handleTouchMove);  // code to re-enable scrolling when modal is inactive for mobile devices
         }
 
         return () => {
             document.body.style.overflow = '';  // code to cleanup and re-enable scrolling when modal is inactive or unmounts
+            document.removeEventListener('touchmove', handleTouchMove);  // code to cleanup and re-enable scrolling when modal is inactive or unmounts for mobile devices
         };
     }, [isOpen]); // here, the dependency array includes isOpen so as to trigger the effect on isOpen change
 
