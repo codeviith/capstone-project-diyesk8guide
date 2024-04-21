22450d864f04d4efbf74194152e05dc9fb597941
KLin <codeviith@gmail.com>
2024-04-20 23:41:31 -0400

    added signup welcome email take 1

--- server/app.py
+++ server/app.py
@@ -9,12 +9,13 @@ from werkzeug.utils import secure_filename
 from PIL import Image
 from io import BytesIO
 from threading import Thread, Event
+from dateutil.parser import isoparse
+from functools import wraps
+from flask_mail import Mail, Message
 import time
 import boto3
 import logging
 import traceback
-from dateutil.parser import isoparse
-from functools import wraps
 
 
 ### Local imports
@@ -82,11 +83,18 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=20)
 ### Initialize Bcrypt
 bcrypt.init_app(app)
 
+app.config['MAIL_SERVER'] = 'smtp.office365.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = os.environ.get('FLASK_MAIL_NAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('FLASK_MAIL_KEY')
+mail = Mail(app)
+
+
 ### configure logging
 # if not app.debug:
 #     app.logger.setLevel(logging.INFO)
-
-
 
 
 
@@ -316,7 +324,57 @@ def signup():
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify({'message': 'Account created successfully'}), 201
+    try:
+        msg = Message("Welcome to DIYeSk8Guide!",
+                    sender="admin@diyesk8guide.com",
+                    recipients=[email])
+        msg.body = f"Hi {fname}, welcome to DIYeSk8Guide! We are excited to have you on board."
+        msg.html =  f"""
+            <html>
+                <head>
+                    <style>
+                        body {{ font-family: Arial, sans-serif; }}
+                        h1 {{ color: #333; }}
+                        ul {{ list-style-type: none; }}
+                        li {{ margin-bottom: 10px; }}
+                        a {{ color: #1a0dab; }}
+                    </style>
+                </head>
+                <body>
+                    <h1>Hi {fname},</h1>
+                    <p>Welcome to <strong>DIYeSk8Guide</strong>, your ultimate resource for building and customizing electric skateboards.</p>
+                    <p>Whether you're crafting your first board or looking to refine your skills, here's what you can find on our platform:</p>
+                    <ul>
+                        <li><strong>Guides:</strong> Immerse yourself in our in-depth Quick Start Guide and specialized tutorials. Each step is crafted to provide you with comprehensive insights, ensuring you have the know-how to assemble your dream board.</li>
+                        <li><strong>Generate:</strong> Utilize our Generate feature to automatically craft a custom skateboard build, tailored to your preferences. Save time and ensure you get the best possible results with our intelligent recommendations.</li>
+                        <li><strong>Guru:</strong> Have questions? Our Esk8 Guru is here to provide instant, reliable advice on everything from component choice to troubleshooting.</li>
+                        <li><strong>Gallery:</strong> Join our vibrant community in the Gallery where you can share your completed projects, gain inspiration, and maybe even be featured in our Hall of Fame!</li>
+                        <li><strong>(Coming Soon!) Video Tutorials:</strong> Watch our expert builders put together different types of boards and explain the intricacies of each part.</li>
+                        <li><strong>(Coming Soon!) Community Insights:</strong> Join discussions with other enthusiasts to share tips, tricks, and board mods.</li>
+                    </ul>
+                    <p>Ready to start? Visit our <a href='https://www.diyesk8guide.com'>website</a> today and explore all that DIYeSk8Guide has to offer.</p>
+                    
+                    <p>We're thrilled to have you on board — let's gear up and skate through the DIY journey together!</p>
+
+                    <p>Happy building!</p>
+                    <p>Warm regards,</p>
+                    <p><strong>The DIYeSk8Guide Team</strong></p>
+                </body>
+            </html>
+            """
+        mail.send(msg)
+        return jsonify({'message': 'Account created successfully'}), 201
+    except Exception as e:
+        return jsonify({'error': 'Failed to send welcome email', 'mesasge':str(e)}), 500
+
+
+
+
+
+
+        # f"<h1>Welcome to DIYeSk8Guide, {fname}!</h1><p>We are excited to have you join our community. Start building today!</p>"
+
+
 
 ### ------------------ USER ------------------ ###
 
